6eb0a531b702cd1580d0cb423eabfc1d43dfeeb5
Kevinbyup84 <kevinmedia84@gmail.com>
2025-02-25 16:44:22 +0700

    update knn

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 import cv2
 import joblib
-import numpy as np 
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+# Load dataset dari file CSV
+color_data = pd.read_csv('colors_may.csv')
+X = color_data[['B', 'G', 'R']].values
+y = color_data['color_name'].values
+
+# Normalisasi Data
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Split dataset untuk training dan testing
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+# Prediksi data test
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+
+# Simpan model dan scaler
+joblib.dump(knn, 'knn_model.pkl')
+joblib.dump(scaler, 'scaler.pkl')
 
 # Muat model KNN dan scaler
 knn = joblib.load('knn_model.pkl')
